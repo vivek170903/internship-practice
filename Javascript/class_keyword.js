7c5b4bba9a71773c1c05c2d30bdb382395cb09d6
vivek170903 <141490296+vivek170903@users.noreply.github.com>
2025-11-27 10:50:53 +0530

    Add files via upload

---
+++ Javascript/class_keyword.js
@@ -0,0 +1,30 @@
+// 2015 / es6 
+// class keyword 
+// class are fake
+
+class CreateUser{
+    constructor(firstName, lastName, email, age, address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.address = address;
+    }
+
+    about(){
+        return `${this.firstName} is ${this.age} years old.`;
+    }
+    is18(){
+        return this.age >= 18;
+    }
+    sing(){
+        return "la la la la ";
+    }
+
+}
+
+
+const user1 = new CreateUser('vivek', 'vashsith', 'vivek@gmail.com', 18, "my address");
+const user2 = new CreateUser('vansh', 'vashsith', 'vansh@gmail.com', 19, "my address");
+const user3 = new CreateUser('mohit', 'vashsitha', 'mohit@gmail.com', 17, "my address");
+// console.log(Object.getPrototypeOf(user1));
---
+++ Javascript/iterables.js
@@ -0,0 +1,18 @@
+// iterables 
+
+// const firstName = "vivek";
+// for(let char of firstName){
+//     console.log(char);
+// }
+
+const items = ['item1', 'item2', 'item3'];
+// for(let item of items){
+//     console.log(item);
+// }
+
+// array like object  
+// example :- string 
+
+// const firstName = "vivek";
+// console.log(firstName.length);
+// console.log(firstName[2]);
