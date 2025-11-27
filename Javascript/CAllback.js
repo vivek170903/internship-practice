9a178feee1969092a45d87f0436e9583cd686c10
vivek170903 <141490296+vivek170903@users.noreply.github.com>
2025-11-27 10:36:17 +0530

    Add files via upload

---
+++ Javascript/CAllback.js
@@ -0,0 +1,12 @@
+function myFunc2(name){
+    console.log("inside my func 2")
+    console.log(`your name is ${name}`);
+}
+
+function myFunc(callback){
+    console.log("hello there I am a func and I can..")
+    callback("harshit");
+}
+
+
+myFunc(myFunc2);
